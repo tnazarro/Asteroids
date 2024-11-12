fa0f2c9813a312a1f90117889c4de75d884388e4
tnazarro <tnazarro@dropletmeasurement.com>
2024-11-12 11:35:45 -0700

    Finished player section

--- main.py
+++ main.py
@@ -3,17 +3,36 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
 
 def main():
 	pygame.init()
 	print("Starting asteroids!")
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	player = Player(x,y, Player.containers)
+
 	while True:
 		for event in pygame.event.get():
 		    if event.type == pygame.QUIT:
 		        return
+
+		for obj in updatable:
+			obj.update(dt)
 		pygame.Surface.fill(screen, (0,0,0))
+		
+		for obj in drawable:
+			obj.draw(screen)
+
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
